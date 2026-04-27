18ec79c7464d16e13046c7f9b3d0ad9e0ec7e353
Benjamin Lee <bbensamuellee@gmail.com>
2026-04-26 18:02:37 -0700

    Add BL_CorrMat_S.R for correlation matrix analysis

---
+++ scripts/supplemental_analyses/BL_CorrMat_S.R
@@ -0,0 +1,73 @@
+```{r}
+library(tidyverse)
+library(phyloseq)
+library(pheatmap)
+library(igraph)
+
+ps = readRDS('Datasets/phyloseq_taxonomy.rds') %>% 
+  tax_glom('Genus') %>%
+  subset_samples(disease == 'MS')
+
+metacyc = read_tsv('Datasets/metacyc.tsv')
+
+ps_rel = ps %>%
+  microbiome::transform('compositional')
+
+# taxa
+
+taxa = as.data.frame(tax_table(ps_rel)) %>%
+  filter(Genus == " g__Succinivibrio" |
+           Genus == " g__Frisingicoccus" |
+           Genus == " g__Erysipelatoclostridium" |
+           Genus == " g__Desulfovibrio") %>%
+  rownames()
+
+ps_filt = prune_taxa(taxa,ps_rel)
+otu = data.frame(ps_filt@otu_table)
+
+# edss
+
+meta = data.frame(sample_data(ps_filt)) %>%
+  select(edss, vitamin.D..IU.) %>%
+  t()
+
+# pathways
+
+metacyc_select = metacyc %>%
+  filter(pathway == 'PWY-6263' |
+           pathway == 'PWY-7371' |
+           pathway == 'PWY-7374' |
+           pathway == 'PWY66-409') %>%
+  column_to_rownames('pathway')
+
+# merge
+
+summary = rbind(otu, 
+                meta, 
+              metacyc_select)
+rownames(summary) = c('Desulfovibrio',
+                      'Succinivibrio',
+                      'Erysipelatoclostridium',
+                      'Frisingicoccus',
+                      'EDSS',
+                      'Vitamin D (IU)',
+                      'superpathway of menaquinol-8 biosynthesis II',
+                      '1,4-dihydroxy-6-naphthoate biosynthesis II',
+                      '1,4-dihydroxy-6-naphthoate biosynthesis I',
+                      'superpathway of purine nucleotide salvage')
+  
+
+# corr calculation
+
+otu_cor = cor(t(summary), method = 'spearman')
+
+corr_mat = pheatmap(otu_cor,
+         clustering_method = "complete",
+         color = colorRampPalette(c("#F2CF7F","white","#AAD9F4"))(50),
+         breaks = seq(-1, 1, length.out = 51),
+         main = "",
+         display_numbers = T,
+         angle_col = "315",
+         fontsize_col = 7.5,
+         fontsize_row = 8.5)                                 
+```
